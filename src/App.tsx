530d5055da793ae6f05f14c0310755ee27126eef
Thomas Collardeau <thomas@collardeau.com>
2018-03-28 20:01:18 +0200

    connect container state component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import * as React from 'react';
 import './App.css';
+const Container = require('react-with-state-props').Container;
 
-const App = () => <div className="App">Hello World</div>;
+type State = object;
+interface Item {
+  title: 'string';
+}
 
-export default App;
+const state: State = {
+  items: {}
+};
+
+const App = (props: State) => {
+  // console.log(props);
+  return <div className="App">Hello World</div>;
+};
+
+const AppContainer = () => <Container state={state} render={App} />;
+
+export default AppContainer;
